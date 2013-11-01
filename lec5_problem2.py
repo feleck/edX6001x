0c988659a3dcb43d802a2c2e2bb80b7e690c9702
Marcin Felczak <feleck@poczta.onet.pl>
2013-11-01 14:42:14 +0100

    l5 p2 done

--- lec5_problem2.py
+++ lec5_problem2.py
@@ -5,7 +5,16 @@ def recurPower(base, exp):
  
     returns: int or float, base^exp
     '''
+
+    #this was my solution:
+    ''' 
     result = 1
     if exp > 0:
         result = base * recurPower(base, exp-1)
     return result
+    '''
+    #this was MIT solution:
+    if exp <= 0:
+        return 1
+
+    return base * recurPower(base, exp - 1)
